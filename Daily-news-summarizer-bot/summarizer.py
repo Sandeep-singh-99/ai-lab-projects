3937d8394f93925a901fc176800fd55be11859a9
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-10-30 09:12:25 +0530

    Refactor summarization logic and update requirements.txt

--- Daily-news-summarizer-bot/summarizer.py
+++ Daily-news-summarizer-bot/summarizer.py
@@ -1,18 +1,37 @@
 from dotenv import load_dotenv
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import OllamaLLM
 from langchain_core.prompts import PromptTemplate
+
+# from langchain_community.prompts import PromptTemplate
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+# llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+llm = OllamaLLM(model="gemma3:1b", temperature=0)
+
+# def summarize_news(news_items):
+#     titles = "\n".join([f" -{n['title']}" for n in news_items])
+#     prompt = PromptTemplate.from_template(
+#         "Summarize the following news headlines in 3-5 points:\n\n{titles}"
+#     )
+
+#     final_prompt = prompt.format(titles=titles)
+#     response = llm.invoke(final_prompt)
+#     return response.content
 
 def summarize_news(news_items):
-    titles = "\n".join([f" -{n['title']}" for n in news_items])
-    prompt = PromptTemplate.from_template(
-        "Summarize the following news headlines in 3-5 points:\n\n{titles}"
-    )
+    titles = "\n".join([f" - {n['title']}" for n in news_items])
+    
+    prompt_template = """
+    Summarize the following news headlines in 3-5 points:
 
+    {titles}
+    """
+    prompt = PromptTemplate.from_template(prompt_template)
     final_prompt = prompt.format(titles=titles)
+    
+    # Use Ollama to generate response
     response = llm.invoke(final_prompt)
-    return response.content
+    return response
